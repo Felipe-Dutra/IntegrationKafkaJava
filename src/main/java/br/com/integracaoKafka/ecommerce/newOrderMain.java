4f2a788875d939b857129c4f45cf08e8ced1c633
Felipe-Dutra <felipedutra98@gmail.com>
2020-09-09 15:35:10 -0300

    criação do properties

---
+++ src/main/java/br/com/integracaoKafka/ecommerce/newOrderMain.java
@@ -0,0 +1,22 @@
+package br.com.integracaoKafka.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class newOrderMain {
+    public static void main(String[] args) {
+       var producer = new KafkaProducer<String,String>(properties());
+    }
+
+    private static Properties properties(){
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1.9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties();
+    }
+}
